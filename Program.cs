ce45bbd27ca7cefa42eda807fe689ed98331b6f6
ava8ha <111045914+ava8ha@users.noreply.github.com>
2022-09-08 11:26:24 +0200

    Add files via upload
    program.cs added

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace LA_319_6908_KontrollstrukturenVerschachtelt
+{
+    class Aufgabe1
+    {
+        static void Main(String[] args)
+        {
+            // Verarbeitung 
+
+            for (int h = 00; h < 24; h++)
+            for (int m = 00; m < 60; m++)
+            for (int s = 00; s < 60; s++)
+
+                    // Ausgabe 
+                    {
+                        Console.WriteLine(h + ":" + m + ":" + s);
+                    }
+        }
+    }
+}                   /*int h = 00; 
+                     * String res = h.ToString("00");
+                     * int m = 00;
+                     * string res1 = m.ToString("00");
+                     * int s = 00;
+                     * string res2 = s.ToString("00");*///System.Thread.Sleep(1000);
+
+
